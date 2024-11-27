f24d02c408d9d901e54f2769d989c152f0d68bf1
Kenneth Taipe <kennethtaipe@gmail.com>
2024-11-26 20:34:34 -0500

    feat:

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
+const os = require('os');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/hostname', (req, res) => {
+    res.send(os.hostname());
 });
 
 app.listen(port, () => {
